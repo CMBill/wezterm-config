6ef268d2a311fad2e543cd648e6c94e96d430720
CMBill <callmebill@billw.cn>
2026-05-07 08:39:55 +0800

    添加 WezTerm 快捷键：垂直拆分和调整窗格大小

--- config/keys.lua
+++ config/keys.lua
@@ -15,6 +15,16 @@ local keys = {
     { key = "UpArrow", mods = "ALT", action = wezterm.action.ActivatePaneDirection "Up" },
     -- Alt+下箭头 -> 向下切换窗格
     { key = "DownArrow", mods = "ALT", action = wezterm.action.ActivatePaneDirection "Down" },
+    -- Alt+\ -> 垂直拆分（下方）
+    { key = "\\", mods = "ALT", action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" } },
+    -- Alt+Shift+左箭头 -> 向左增大窗格
+    { key = "LeftArrow", mods = "ALT|SHIFT", action = wezterm.action.AdjustPaneSize { "Left", 3 } },
+    -- Alt+Shift+右箭头 -> 向右增大窗格
+    { key = "RightArrow", mods = "ALT|SHIFT", action = wezterm.action.AdjustPaneSize { "Right", 3 } },
+    -- Alt+Shift+上箭头 -> 向上增大窗格
+    { key = "UpArrow", mods = "ALT|SHIFT", action = wezterm.action.AdjustPaneSize { "Up", 3 } },
+    -- Alt+Shift+下箭头 -> 向下增大窗格
+    { key = "DownArrow", mods = "ALT|SHIFT", action = wezterm.action.AdjustPaneSize { "Down", 3 } },
     -- Alt+x -> 关闭当前窗格
     { key = "x", mods = "ALT", action = wezterm.action.CloseCurrentPane { confirm = true } },
   },
